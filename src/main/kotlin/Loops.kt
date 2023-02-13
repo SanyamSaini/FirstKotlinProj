1cb2d62690623fa4939bf21f067ddbe65c773522
Sanyam Saini <sanyamsaini51@gmail.com>
2023-02-13 19:27:17 +0530

    While & do While

---
+++ src/main/kotlin/Loops.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var count = 5
+
+    do {
+        println("Running just once")
+    } while (count > 5)
+
+    while (count >= 1) {
+        println("Hello")
+        count--
+    }
+
+
+}
